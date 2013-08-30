c81edd9cdc5327af8689447e54786e71161e7876
MarsLuo <lyyx_0@163.com>
2013-08-30 14:09:04 +0800

    remove holiday project; add support for iOS 6.0, change to ARC

--- Examples/NativeCal/Classes/EventKitDataSource.m
+++ Examples/NativeCal/Classes/EventKitDataSource.m
@@ -8,7 +8,7 @@
 
 static BOOL IsDateBetweenInclusive(NSDate *date, NSDate *begin, NSDate *end)
 {
-  return [date compare:begin] != NSOrderedAscending && [date compare:end] != NSOrderedDescending;
+    return [date compare:begin] != NSOrderedAscending && [date compare:end] != NSOrderedDescending;
 }
 
 @interface EventKitDataSource ()
@@ -20,113 +20,135 @@ static BOOL IsDateBetweenInclusive(NSDate *date, NSDate *begin, NSDate *end)
 
 + (EventKitDataSource *)dataSource
 {
-  return [[[[self class] alloc] init] autorelease];
+    return [[[self class] alloc] init];
 }
 
 - (id)init
 {
-  if ((self = [super init])) {
-    eventStore = [[EKEventStore alloc] init];
-    events = [[NSMutableArray alloc] init];
-    items = [[NSMutableArray alloc] init];
-    eventStoreQueue = dispatch_queue_create("com.thepolypeptides.nativecalexample", NULL);
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(eventStoreChanged:) name:EKEventStoreChangedNotification object:nil];
-  }
-  return self;
+    if ((self = [super init])) {
+        eventStore = [[EKEventStore alloc] init];
+        events = [[NSMutableArray alloc] init];
+        items = [[NSMutableArray alloc] init];
+        eventStoreQueue = dispatch_queue_create("com.thepolypeptides.nativecalexample", NULL);
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(eventStoreChanged:) name:EKEventStoreChangedNotification object:nil];
+    }
+    return self;
 }
 
 - (void)eventStoreChanged:(NSNotification *)note
 {
-  [[NSNotificationCenter defaultCenter] postNotificationName:KalDataSourceChangedNotification object:nil];
+    [eventStore requestAccessToEntityType:EKEntityTypeEvent completion:^(BOOL granted, NSError *error) {
+        if (granted) {
+            [[NSNotificationCenter defaultCenter] postNotificationName:KalDataSourceChangedNotification object:nil];
+        }
+    }];
 }
 
 - (EKEvent *)eventAtIndexPath:(NSIndexPath *)indexPath
 {
-  return [items objectAtIndex:indexPath.row];
+    return [items objectAtIndex:indexPath.row];
 }
 
 #pragma mark UITableViewDataSource protocol conformance
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-  static NSString *identifier = @"MyCell";
-  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
-  if (!cell) {
-    cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier] autorelease];
-    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-    cell.selectionStyle = UITableViewCellSelectionStyleNone;
-    cell.imageView.contentMode = UIViewContentModeScaleAspectFill;
-  }
+    static NSString *identifier = @"MyCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        cell.imageView.contentMode = UIViewContentModeScaleAspectFill;
+    }
 
-  EKEvent *event = [self eventAtIndexPath:indexPath];
-  cell.textLabel.text = event.title;
-  return cell;
+    EKEvent *event = [self eventAtIndexPath:indexPath];
+    cell.textLabel.text = event.title;
+    return cell;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-  return [items count];
+    return [items count];
 }
 
 #pragma mark KalDataSource protocol conformance
 
 - (void)presentingDatesFrom:(NSDate *)fromDate to:(NSDate *)toDate delegate:(id<KalDataSourceCallbacks>)delegate
 {
-  // asynchronous callback on the main thread
-  [events removeAllObjects];
-  NSLog(@"Fetching events from EventKit between %@ and %@ on a GCD-managed background thread...", fromDate, toDate);
-  dispatch_async(eventStoreQueue, ^{
-    NSDate *fetchProfilerStart = [NSDate date];
-    NSPredicate *predicate = [eventStore predicateForEventsWithStartDate:fromDate endDate:toDate calendars:nil];
-    NSArray *matchedEvents = [eventStore eventsMatchingPredicate:predicate];
-    dispatch_async(dispatch_get_main_queue(), ^{
-      NSLog(@"Fetched %d events in %f seconds", [matchedEvents count], -1.f * [fetchProfilerStart timeIntervalSinceNow]);
-      [events addObjectsFromArray:matchedEvents];
-      [delegate loadedDataSource:self];
+    // asynchronous callback on the main thread
+    [events removeAllObjects];
+    NSLog(@"Fetching events from EventKit between %@ and %@ on a GCD-managed background thread...", fromDate, toDate);
+    dispatch_async(eventStoreQueue, ^{
+        NSDate *fetchProfilerStart = [NSDate date];
+        // Get the appropriate calendar
+        NSCalendar *calendar = [NSCalendar currentCalendar];
+
+        // Create the start date components
+        NSDateComponents *oneDayAgoComponents = [[NSDateComponents alloc] init];
+        oneDayAgoComponents.day = -1;
+        NSDate *oneDayAgo = [calendar dateByAddingComponents:oneDayAgoComponents
+                                                      toDate:[NSDate date]
+                                                     options:0];
+
+        // Create the end date components
+        NSDateComponents *oneYearFromNowComponents = [[NSDateComponents alloc] init];
+        oneYearFromNowComponents.year = 1;
+        NSDate *oneYearFromNow = [calendar dateByAddingComponents:oneYearFromNowComponents
+                                                           toDate:[NSDate date]
+                                                          options:0];
+
+        // Create the predicate from the event store's instance method
+        NSPredicate *predicate = [eventStore predicateForEventsWithStartDate:oneDayAgo
+                                                                     endDate:oneYearFromNow
+                                                                   calendars:nil];
+
+        // Fetch all events that match the predicate
+        NSArray *matchedEvents = [eventStore eventsMatchingPredicate:predicate];
+
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSLog(@"Fetched %d events in %f seconds", [matchedEvents count], -1.f * [fetchProfilerStart timeIntervalSinceNow]);
+            [events addObjectsFromArray:matchedEvents];
+            [delegate loadedDataSource:self];
+        });
     });
-  });
 }
 
 - (NSArray *)markedDatesFrom:(NSDate *)fromDate to:(NSDate *)toDate
 {
-  // synchronous callback on the main thread
-  return [[self eventsFrom:fromDate to:toDate] valueForKeyPath:@"startDate"];
+    // synchronous callback on the main thread
+    return [[self eventsFrom:fromDate to:toDate] valueForKeyPath:@"startDate"];
 }
 
 - (void)loadItemsFromDate:(NSDate *)fromDate toDate:(NSDate *)toDate
 {
-  // synchronous callback on the main thread
-  [items addObjectsFromArray:[self eventsFrom:fromDate to:toDate]];
+    // synchronous callback on the main thread
+    [items addObjectsFromArray:[self eventsFrom:fromDate to:toDate]];
 }
 
 - (void)removeAllItems
 {
-  // synchronous callback on the main thread
-  [items removeAllObjects];
+    // synchronous callback on the main thread
+    [items removeAllObjects];
 }
 
 #pragma mark -
 
 - (NSArray *)eventsFrom:(NSDate *)fromDate to:(NSDate *)toDate
 {
-  NSMutableArray *matches = [NSMutableArray array];
-  for (EKEvent *event in events)
-    if (IsDateBetweenInclusive(event.startDate, fromDate, toDate))
-      [matches addObject:event];
-  
-  return matches;
+    NSMutableArray *matches = [NSMutableArray array];
+    for (EKEvent *event in events)
+        if (IsDateBetweenInclusive(event.startDate, fromDate, toDate))
+            [matches addObject:event];
+    
+    return matches;
 }
 
 - (void)dealloc
 {
-  [[NSNotificationCenter defaultCenter] removeObserver:self name:EKEventStoreChangedNotification object:nil];
-  [items release];
-  [events release];
-  dispatch_sync(eventStoreQueue, ^{
-    [eventStore release];
-  });
-  dispatch_release(eventStoreQueue);
-  [super dealloc];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:EKEventStoreChangedNotification object:nil];
+    dispatch_sync(eventStoreQueue, ^{
+    });
 }
 
 @end
